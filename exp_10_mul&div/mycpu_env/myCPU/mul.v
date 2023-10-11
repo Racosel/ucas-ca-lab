b4b23e2d07b93e6a6ae2216bfedbed92e4d9ca96
nakalama <xuchangjie21@mails.ucas.ac.cn>
2023-10-11 15:44:12 +0800

    add:mul but use ip

--- exp_10_mul&div/mycpu_env/myCPU/mul.v
+++ exp_10_mul&div/mycpu_env/myCPU/mul.v
@@ -28,11 +28,11 @@ input [31:0] x,
 input [31:0] y,
 output[63:0]result
 );
-wire [32:0] extended_x;
-wire [32:0] extended_y;
-wire [63:0] tempans;
-assign extended_x = {mul_signed & x[31],x};
-assign extended_y = {mul_signed & y[31],y};
-assign tempans = $signed(extended_x) * $signed(extended_y) ;
-assign result = tempans;
+    wire [32:0] extended_x;
+    wire [32:0] extended_y;
+    wire [63:0] tempans;
+    assign extended_x = {mul_signed & x[31],x};
+    assign extended_y = {mul_signed & y[31],y};
+    assign tempans = $signed(extended_x) * $signed(extended_y) ;
+    assign result = tempans;
 endmodule
